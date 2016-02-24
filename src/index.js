38557797c508721540cf058ab9cda7d1aee698fe
Daniel Stanzani <daniel@stanzani.com.br>
2016-02-24 00:15:05 -0300

    New examples of Riot using callbacks, events, loops and ajax

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import riot from 'riot';
-import hey from './tags/hey.tag';
-document.addEventListener('DOMContentLoaded', () => riot.mount('hey'));
+import './tags/contact-list.tag';
+document.addEventListener('DOMContentLoaded', () => riot.mount('hey-form'));
+document.addEventListener('DOMContentLoaded', () => riot.mount('contact-list', {callback:tagCallback}));
+
+function tagCallback(theTag){
+  console.log('callback executed');
+  var request = new XMLHttpRequest();
+  request.open('GET', 'people.json', true);
+  request.onload = ()=> {
+    if(request.status == 200){
+      var data = JSON.parse(request.responseText);
+      theTag.trigger('data_loaded', data.people);
+    }
+  };
+  request.send();
+}
--- src/tags/hey.tag
+++
@@ -1,3 +0,0 @@
-<hey>
-  <h1>Heeeey Brother!</h1>
-</hey>
